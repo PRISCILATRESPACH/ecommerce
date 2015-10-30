12c8d9298a39477f2e5376795827cbe0aa95ce10
Priscila <priscilatrespach@gmail.com>
2015-10-29 22:40:01 -0200

    implementado as classes livro, pedidos, categoria, pagamento e usuario

---
+++ ecommerce/src/main/java/br/univel/ecommerce/CadLivro.java
@@ -0,0 +1,216 @@
+package br.univel.ecommerce;
+
+import javax.persistence.Entity;
+import java.io.Serializable;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Column;
+import javax.persistence.Version;
+import java.lang.Override;
+import br.univel.ecommerce.CadCategoria;
+import javax.persistence.ManyToOne;
+import javax.persistence.FetchType;
+import javax.persistence.CascadeType;
+
+@Entity
+public class CadLivro implements Serializable
+{
+
+   @Id
+   @GeneratedValue(strategy = GenerationType.AUTO)
+   @Column(name = "id", updatable = false, nullable = false)
+   private Long id;
+   @Version
+   @Column(name = "version")
+   private int version;
+
+   @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+   private CadCategoria CadCategoria;
+
+   @Column
+   private String titulo;
+
+   @Column
+   private int paginas;
+
+   @Column
+   private String ISBN;
+
+   @Column
+   private String autor;
+
+   @Column
+   private String editora;
+
+   @Column
+   private String quantidade;
+
+   @Column
+   private double preco;
+
+   @Column
+   private String imagem;
+
+   public Long getId()
+   {
+      return this.id;
+   }
+
+   public void setId(final Long id)
+   {
+      this.id = id;
+   }
+
+   public int getVersion()
+   {
+      return this.version;
+   }
+
+   public void setVersion(final int version)
+   {
+      this.version = version;
+   }
+
+   @Override
+   public boolean equals(Object obj)
+   {
+      if (this == obj)
+      {
+         return true;
+      }
+      if (!(obj instanceof CadLivro))
+      {
+         return false;
+      }
+      CadLivro other = (CadLivro) obj;
+      if (id != null)
+      {
+         if (!id.equals(other.id))
+         {
+            return false;
+         }
+      }
+      return true;
+   }
+
+   @Override
+   public int hashCode()
+   {
+      final int prime = 31;
+      int result = 1;
+      result = prime * result + ((id == null) ? 0 : id.hashCode());
+      return result;
+   }
+
+   public CadCategoria getCadCategoria()
+   {
+      return this.CadCategoria;
+   }
+
+   public void setCadCategoria(final CadCategoria CadCategoria)
+   {
+      this.CadCategoria = CadCategoria;
+   }
+
+   public String getTitulo()
+   {
+      return titulo;
+   }
+
+   public void setTitulo(String titulo)
+   {
+      this.titulo = titulo;
+   }
+
+   public int getPaginas()
+   {
+      return paginas;
+   }
+
+   public void setPaginas(int paginas)
+   {
+      this.paginas = paginas;
+   }
+
+   public String getISBN()
+   {
+      return ISBN;
+   }
+
+   public void setISBN(String ISBN)
+   {
+      this.ISBN = ISBN;
+   }
+
+   public String getAutor()
+   {
+      return autor;
+   }
+
+   public void setAutor(String autor)
+   {
+      this.autor = autor;
+   }
+
+   public String getEditora()
+   {
+      return editora;
+   }
+
+   public void setEditora(String editora)
+   {
+      this.editora = editora;
+   }
+
+   public String getQuantidade()
+   {
+      return quantidade;
+   }
+
+   public void setQuantidade(String quantidade)
+   {
+      this.quantidade = quantidade;
+   }
+
+   public double getPreco()
+   {
+      return preco;
+   }
+
+   public void setPreco(double preco)
+   {
+      this.preco = preco;
+   }
+
+   public String getImagem()
+   {
+      return imagem;
+   }
+
+   public void setImagem(String imagem)
+   {
+      this.imagem = imagem;
+   }
+
+   @Override
+   public String toString()
+   {
+      String result = getClass().getSimpleName() + " ";
+      if (titulo != null && !titulo.trim().isEmpty())
+         result += "titulo: " + titulo;
+      result += ", paginas: " + paginas;
+      if (ISBN != null && !ISBN.trim().isEmpty())
+         result += ", ISBN: " + ISBN;
+      if (autor != null && !autor.trim().isEmpty())
+         result += ", autor: " + autor;
+      if (editora != null && !editora.trim().isEmpty())
+         result += ", editora: " + editora;
+      if (quantidade != null && !quantidade.trim().isEmpty())
+         result += ", quantidade: " + quantidade;
+      result += ", preco: " + preco;
+      if (imagem != null && !imagem.trim().isEmpty())
+         result += ", imagem: " + imagem;
+      return result;
+   }
+}
